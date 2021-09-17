e7d848c588a5194989625b7419a3f9b7009b9673
Gaurav Samaria <gaurav.samaria17@gmail.com>
2021-09-17 22:34:11 +0530

    Add files via upload

---
+++ 1368A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        long long a, b, n;
+        long count=0;
+        cin>>a>>b>>n;
+        while(a<=n && b<=n){
+            if(a>b){
+                b=a+b;
+                count++;
+            }
+            else{
+                a=a+b;
+                count++;
+            }
+        }
+        cout<<count<<"\n";
+    }
+	return 0;
+}
+
